972b7cf40207d2774a403d25367b45e3e52a07fb
t.takahashi <takahashi.developer@gmail.com>
2020-09-09 01:36:11 +0900

    fix token expired

--- spotify_api_client.py
+++ spotify_api_client.py
@@ -3,20 +3,21 @@
 import os
 
 import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 
 class SpotifyApiClient():
   def __init__(self, scope):
     username = os.environ['SPOTIPY_USERNAME']
-    self.client = self.get_spotify_api_client(username, scope)
+    self.__auth_manager = SpotifyOAuth(username=username, scope=scope)
+    self.__client = spotipy.Spotify(auth_manager=self.__auth_manager)
 
-  def get_spotify_api_client(self, username, scope):
-    token = spotipy.util.prompt_for_user_token(
-        username, scope=scope)
-    return spotipy.Spotify(auth=token)
+  def get_client(self):
+    self.__auth_manager.get_access_token(as_dict=False)
+    return self.__client
 
   def get_now_playing_track(self):
-    track = self.client.current_user_playing_track()
+    track = self.get_client().current_user_playing_track()
     if track == None:
       return None
     title = track['item']['name']
